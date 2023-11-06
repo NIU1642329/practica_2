58e2af8c31ab660eea392e3861743da0aaadb796
Laia Alcalde Maria <laiaalcaldemaria@Macintosh.local>
2023-11-06 08:49:34 +0100

    30/10_9:46

--- p33.c
+++ p33.c
@@ -5,33 +5,28 @@
 
 float Mat[N][N];
 float MatDD[N][N];
-float V1[N], V2[N], V3[N];
+float V1[N];
+float V2[N];
+float V3[N];
 
 void InitData(){
-    int i, j;
-    srand(4422543);
-
-    for (i = 0; i < N; i++) {
-        for (j = 0; j < N; j++) {
-            Mat[i][j] = (((i * j) % 3) ? -1 : 1) * (100.0 * (rand() / (1.0 * RAND_MAX)));
-
-            if ((abs(i - j) <= 3) && (i != j)) {
-                MatDD[i][j] = (((i * j) % 3) ? -1 : 1) * (rand() / (1.0 * RAND_MAX));
-            } else if (i == j) {
-                MatDD[i][j] = (((i * j) % 3) ? -1 : 1) * (10000.0 * (rand() / (1.0 * RAND_MAX)));
-            } else {
-                MatDD[i][j] = 0.0;
-            }
-        }
-    }
-
-    for (i = 0; i < N; i++) {
-        V1[i] = (i < N / 2) ? (((i * j) % 3) ? -1 : 1) * (100.0 * (rand() / (1.0 * RAND_MAX))) : 0.0;
-        V2[i] = (i >= N / 2) ? (((i * j) % 3) ? -1 : 1) * (100.0 * (rand() / (1.0 * RAND_MAX))) : 0.0;
-        V3[i] = (((i * j) % 5) ? -1 : 1) * (100.0 * (rand() / (1.0 * RAND_MAX)));
-    }
+	int i,j;
+	srand(4422543);
+	for( i = 0; i < N; i++ )
+		for( j = 0; j < N; j++ ){
+			Mat[i][j]=(((i*j)%3)?-1:1)*(100.0*(rand()/(1.0*RAND_MAX)));
+			if ( (abs(i - j) <= 3) && (i != j))
+				MatDD[i][j] = (((i*j)%3) ? -1 : 1)*(rand()/(1.0*RAND_MAX));
+			else if ( i == j )
+				MatDD[i][j]=(((i*j)%3)?-1:1)*(10000.0*(rand()/(1.0*RAND_MAX)));
+			else MatDD[i][j] = 0.0;
+		}
+	for( i = 0; i < N; i++ ){
+		V1[i]=(i<N/2)?(((i*j)%3)?-1:1)*(100.0*(rand()/(1.0*RAND_MAX))):0.0;
+		V2[i]=(i>=N/2)?(((i*j)%3)?-1:1)*(100.0*(rand()/(1.0*RAND_MAX))):0.0;
+		V3[i]=(((i*j)%5)?-1:1)*(100.0*(rand()/(1.0*RAND_MAX)));
+	}
 }
-
 void PrintVect(float vect[N], int from, int numel) {
     /*if (from < 0 || from >= N || numel <= 0 || from + numel > N) {
         printf("Error: Índex fora dels límits del vector.\n");
@@ -100,13 +95,14 @@ int main() {
     PrintVect(ResultVector, 0, 10);
     PrintVect(ResultVector, 90, 10);
    
-    printf("El producto escalar de los dos vectores es:\n");
-    Scalar(V1, V2);
-    
-    float vectorToCheck[N];
-    float magnitude = Magnitude(vectorToCheck);
-    printf("La magnitud del vector es: %f\n", magnitude);
-    
+    float V1Scaled[N];
+    float alpha = 2.0;
+    MultEscalar(V1, V1Scaled, alpha);
+    printf("Vector V1Scaled (resultado de la multiplicación escalar):\n");
+    PrintVect(V1Scaled, 0, 10);
+    float dotProduct = Scalar(V1, V1Scaled);
+    printf("Producto escalar entre V1 y V1Scaled: %f\n", dotProduct);
+
     return 0;
 }
 
